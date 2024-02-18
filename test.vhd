b2a881e4130481707d5895400415402d08d3b604
mateuszkowalczyk0305 <160405673+mateuszkowalczyk0305@users.noreply.github.com>
2024-02-18 06:34:46 -0800

    Adding old students project

---
+++ test.vhd
@@ -0,0 +1,109 @@
+----------------------------------------------------------------------------------
+-- Autor: Mateusz Kowalczyk
+-- Numer Indeksu: 268533
+-- Data oddania projektu: 26.01.2024.r
+-- Temat: Kalkulator - test bench
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_TEXTIO.ALL;
+use std.textio.all;
+
+entity alu_tb is
+
+end alu_tb;
+
+architecture behavior of alu_tb is
+
+    component simple_alu
+    Port ( -- wejœcia
+			  A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+           operator : in STD_LOGIC_VECTOR (1 downto 0);
+			  clk : in std_logic;
+			  reset: in std_logic;
+			  -- wyjscia
+           result : out STD_LOGIC_VECTOR (7 downto 0));
+    end component;
+
+-- wejœcia
+   signal A : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
+   signal B : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
+   signal operator : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
+	signal clk : STD_LOGIC := '0';
+	signal reset : STD_LOGIC := '0';
+-- wyjœcia
+   signal result : STD_LOGIC_VECTOR (7 downto 0);
+
+   file result_file : text is out "wyniki.txt";
+	
+	constant clk_period : time := 10 ns;
+	
+
+begin
+
+   uut: simple_alu port map (
+        A => A,
+        B => B,
+        operator => operator,
+		  clk => clk,
+		  reset => reset,
+        result => result
+   );
+	-- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+
+
+   stim_proc: process
+   variable l: line; -- przypisanie "line" do zmiennej "1"
+   begin	
+
+		reset <= '1';
+		wait for clk_period*5;
+		
+		reset <= '0';
+		wait for clk_period*5;
+		
+		reset <= '1';
+		wait for clk_period*5;
+		
+      A <= "0010"; B <= "0011"; operator <= "00";
+      wait for clk_period*5;
+      write(l, string'("Dodawanie Result: "));
+      write(l, result);
+      writeline(result_file, l);
+
+      A <= "0100"; B <= "0011"; operator <= "01";
+      wait for clk_period*5;
+      write(l, string'("Odejmowanie Result: "));
+      write(l, result);
+      writeline(result_file, l);
+
+      A <= "0011"; B <= "0010"; operator <= "10";
+      wait for clk_period*5;
+      write(l, string'("Mnozenie Result: "));
+      write(l, result);
+      writeline(result_file, l);
+
+      A <= "0100"; B <= "0010"; operator <= "11";
+      wait for clk_period*5;
+      write(l, string'("Dzielenie Result: "));
+      write(l, result);
+      writeline(result_file, l);
+
+      A <= "0100"; B <= "0000"; operator <= "11";
+      wait for clk_period*5;
+      write(l, string'("Dzielenie Przez Zero Result: "));
+      write(l, result);
+      writeline(result_file, l);
+
+      wait;
+   end process;
+
+end behavior;
---
+++ uut.vhd
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------------------
+-- Autor: Mateusz Kowalczyk
+-- Numer Indeksu: 268533
+-- Data oddania projektu: 26.01.2024.r
+-- Temat: Kalkulator - module file
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity simple_alu is
+    Port ( 
+			-- input
+			A : in STD_LOGIC_VECTOR (3 downto 0);
+         B : in STD_LOGIC_VECTOR (3 downto 0);
+         operator : in STD_LOGIC_VECTOR (1 downto 0);
+			clk : in std_logic;
+			reset: in std_logic;
+			-- output
+         result : out STD_LOGIC_VECTOR (7 downto 0));
+			
+end simple_alu;
+
+architecture Behavioral of simple_alu is
+begin
+    process(A, B, operator, clk, reset)
+    begin
+	 
+	 if rising_edge(clk) and reset = '1' then
+		case operator is
+			 when "00" => -- dodawanie
+				  result <= "0000" & std_logic_vector(unsigned(A) + unsigned(B));
+			 when "01" => -- odejmowanie
+				  result <= "0000" & std_logic_vector(unsigned(A) - unsigned(B));
+			 when "10" => -- mno¿enie
+				  result <= std_logic_vector(unsigned(A) * unsigned(B));
+			 when "11" => -- dzielenie ze sprawdzeniem czy nie dzielimy przez zero
+				  if B /= "0000" and A /= "0000" then
+						result <= "0000" & std_logic_vector(unsigned(A) / unsigned(B));
+				  else
+						result <= "00000000"; 
+				  end if;
+			 when others =>
+				  result <= (others => '0'); 
+		end case;
+		end if;
+
+    end process;
+end Behavioral;
+
+
+
+
